e495e63d13426b81609d16945c156ed6c5ea302a
MohamedWaleed99 <mowaled639@gmail.com>
2026-06-10 01:29:50 +0300

    feature(Shorebird):the Shorebird feature is finished

--- lib/features/home/presentation/widgets/category_view.dart
+++ lib/features/home/presentation/widgets/category_view.dart
@@ -5,10 +5,9 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
-import 'package:ionicons/ionicons.dart';
-
 import '../../../../core/navigation/app_routes.dart';
 import '../view_models/home_view_model.dart';
+
 class CategoryView extends GetView<HomeViewModel> {
   const CategoryView({super.key});
 
@@ -17,102 +16,126 @@ class CategoryView extends GetView<HomeViewModel> {
     final theme = Theme.of(context);
 
     return Obx(
-          () => SizedBox(
+      () => SizedBox(
         height: 110.h,
         child: controller.categories.isEmpty
             ? const Center(child: CircularProgressIndicator())
             : ListView.separated(
-          padding: REdgeInsets.symmetric(horizontal: 16),
-          scrollDirection: Axis.horizontal,
-          physics: const BouncingScrollPhysics(),
-          itemCount: controller.categories.length,
-          separatorBuilder: (_, __) => SizedBox(width: 20.w),
-          itemBuilder: (context, index) {
-            final category = controller.categories[index];
+                padding: REdgeInsets.symmetric(horizontal: 16),
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                itemCount: controller.categories.length,
+                separatorBuilder: (_, __) => SizedBox(width: 20.w),
+                itemBuilder: (context, index) {
+                  final category = controller.categories[index];
 
-            return GestureDetector(
-              onTap: () {
-
-                Get.toNamed(
-                  AppRoutes.categoryProducts,
-                  arguments: category,
-                );
-              },
-              child: SizedBox(
-                width: 75.w,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      width: 68.r,
-                      height: 68.r,
-                      decoration: BoxDecoration(
-                        color: theme.primaryColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(22.r),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          _getCategoryIcon(category.slug),
-                          color: theme.primaryColor,
-                          size: 28.sp,
-                        ),
+                  return GestureDetector(
+                    onTap: () {
+                      Get.toNamed(
+                        AppRoutes.categoryProducts,
+                        arguments: category,
+                      );
+                    },
+                    child: SizedBox(
+                      width: 75.w,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            width: 68.r,
+                            height: 68.r,
+                            decoration: BoxDecoration(
+                              color: theme.primaryColor.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(22.r),
+                            ),
+                            child: Center(
+                              child: Icon(
+                                _getCategoryIcon(category.slug),
+                                color: theme.primaryColor,
+                                size: 28.sp,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 8.h),
+                          Text(
+                            category.name ?? "",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                            style: theme.textTheme.bodySmall?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: theme.colorScheme.onSurface,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 8.h),
-                    Text(
-                      category.name ?? "",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: theme.textTheme.bodySmall?.copyWith(
-                        fontWeight: FontWeight.w600,
-                        color: theme.colorScheme.onSurface,
-                      ),
-                    ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ),
     );
   }
 
-
   IconData _getCategoryIcon(String? slug) {
     switch (slug) {
-      case 'beauty': return Ionicons.sparkles_outline;
-      case 'fragrances': return Ionicons.flask_outline;
-      case 'skin-care': return Ionicons.water_outline;
+      case 'beauty':
+        return Icons.auto_awesome_outlined;
+      case 'fragrances':
+        return Icons.science_outlined;
+      case 'skin-care':
+        return Icons.water_drop_outlined;
 
-      case 'furniture': return Ionicons.bed_outline;
-      case 'groceries': return Ionicons.basket_outline;
-      case 'home-decoration': return Ionicons.home_outline;
-      case 'kitchen-accessories': return Ionicons.restaurant_outline;
+      case 'furniture':
+        return Icons.bed_outlined;
+      case 'groceries':
+        return Icons.shopping_basket_outlined;
+      case 'home-decoration':
+        return Icons.home_outlined;
+      case 'kitchen-accessories':
+        return Icons.restaurant_outlined;
 
-      case 'laptops': return Ionicons.laptop_outline;
-      case 'tablets': return Ionicons.tablet_landscape_outline;
-      case 'smartphones': return Ionicons.phone_portrait_outline;
-      case 'mobile-accessories': return Ionicons.bluetooth_outline;
+      case 'laptops':
+        return Icons.laptop_outlined;
+      case 'tablets':
+        return Icons.tablet_android_outlined;
+      case 'smartphones':
+        return Icons.smartphone_outlined;
+      case 'mobile-accessories':
+        return Icons.bluetooth_outlined;
 
-      case 'mens-shirts': return Ionicons.shirt_outline;
-      case 'mens-shoes': return Ionicons.walk_outline;
-      case 'mens-watches': return Ionicons.watch_outline;
+      case 'mens-shirts':
+        return Icons.checkroom_outlined;
+      case 'mens-shoes':
+        return Icons.directions_walk_outlined;
+      case 'mens-watches':
+        return Icons.watch_outlined;
 
-      case 'womens-bags': return Ionicons.bag_handle_outline;
-      case 'womens-dresses': return Ionicons.woman_outline;
-      case 'womens-jewellery': return Ionicons.diamond_outline;
-      case 'womens-shoes': return Ionicons.footsteps_outline;
-      case 'womens-watches': return Ionicons.time_outline;
+      case 'womens-bags':
+        return Icons.local_mall_outlined;
+      case 'womens-dresses':
+        return Icons.woman_outlined;
+      case 'womens-jewellery':
+        return Icons.diamond_outlined;
+      case 'womens-shoes':
+        return Icons.shopping_bag_outlined;
+      case 'womens-watches':
+        return Icons.access_time_outlined;
 
-      case 'tops': return Ionicons.body_outline;
-      case 'sunglasses': return Ionicons.glasses_outline;
-      case 'sports-accessories': return Ionicons.football_outline;
+      case 'tops':
+        return Icons.accessibility_new_outlined;
+      case 'sunglasses':
+        return Icons.wb_sunny_outlined;
+      case 'sports-accessories':
+        return Icons.sports_soccer_outlined;
 
-      case 'motorcycle': return Ionicons.bicycle_outline;
-      case 'vehicle': return Ionicons.car_sport_outline;
+      case 'motorcycle':
+        return Icons.motorcycle_outlined;
+      case 'vehicle':
+        return Icons.directions_car_outlined;
 
-      default: return Ionicons.apps_outline;
+      default:
+        return Icons.apps_outlined;
     }
-  }}
+  }
+}
